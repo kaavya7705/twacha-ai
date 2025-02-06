e0e5d9c4f99c703e38aa544106323e1213c5ecd7
kaavya7705 <145352258+kaavya7705@users.noreply.github.com>
2025-02-07 01:10:18 +0530

    ....

---
+++ t/src/app/api/uploadthing/core.ts
@@ -0,0 +1,13 @@
+import { createUploadthing, type FileRouter } from "uploadthing/server";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  imageUploader: f({ image: { maxFileSize: "4MB" } })
+    .onUploadComplete(({ file }) => {
+      console.log("File uploaded:", file.url);
+      return { url: file.url };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
